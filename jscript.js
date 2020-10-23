8606ed180256e84b7c266b3f7cfc6001002bc05e
Juga limbu <labungjugal1@gmail.com>
2020-10-23 21:39:23 +0200

    got the image from wordpress

--- jscript.js
+++ jscript.js
@@ -1,2 +1,40 @@
 "use strict";
-console.log("hello")
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    console.log("hello")
+    loadjson();
+
+}
+async function loadjson(){
+    fetch("https://www.nepanime.dk/wp-json/wp/v2/Gallery/?_embed")
+		.then(res => res.json())
+		.then(handleData)
+}
+function handleData (myData) {
+    //1. loop the data 
+    myData.forEach(showVase);
+
+}
+function showVase(vase){
+console.log(vase);
+const imgPath = vase._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+
+    // 2. cloning template
+    const template = document.querySelector(".myTemplate").content;
+    const vaseCopy = template.cloneNode(true);
+
+    // 3. cloning the data 
+    vaseCopy.querySelector(".title").innerHTML = vase.artist
+    vaseCopy.querySelector(".title").innerHTML = vase.artist
+    vaseCopy.querySelector(".desc").innerText   = vase.description
+    vaseCopy.querySelector(".vase-size").innerText   = vase.size
+
+   const img = vaseCopy.querySelector("img");
+	img.setAttribute("src", imgPath);
+     img.setAttribute("alt", "image of the vase");
+    
+    // 4. append 
+    document.querySelector(".vases").appendChild(vaseCopy);
+}
+
